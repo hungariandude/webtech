59dd04dba801db5b8a084824c23602dddd7e6aac
hungariandude <hungariandude@users.noreply.github.com>
2016-04-10 22:27:16 +0200

    Indent with spaces in JS source file.

--- nir-server/src/main/resources/static/nir.js
+++ nir-server/src/main/resources/static/nir.js
@@ -1,59 +1,59 @@
 $(document).ready(function() {
-	$("#purchaseButton").click(function() {
-		$.soap({
-			url : 'http://localhost:8080/ws/',
-			method : 'newPurchaseRequest',
-			namespaceURL : 'http://spring.io/guides/gs-producing-web-service',
+    $("#purchaseButton").click(function() {
+        $.soap({
+            url : 'http://localhost:8080/ws/',
+            method : 'newPurchaseRequest',
+            namespaceURL : 'http://spring.io/guides/gs-producing-web-service',
 
-			data : {
-				purchase : {
-					customerId : $("#customersSelect").val(),
-					productId : $("#productsSelect").val(),
-					quantity : $("#quantityInput").val()
-				}
-			},
+            data : {
+                purchase : {
+                    customerId : $("#customersSelect").val(),
+                    productId : $("#productsSelect").val(),
+                    quantity : $("#quantityInput").val()
+                }
+            },
 
-			success : function(soapResponse) {
-				var xmlStr = soapResponse.toString();
-				var xmlDoc = $.parseXML(xmlStr);
-				$xml = $(xmlDoc);
-				var errorStr = $xml.find("error").text();
-				if (errorStr) {
-					alert(errorStr);
-				} else {
-					location.reload();
-				}
-			},
-			error : function(soapResponse) {
-				// show error
-				alert("Hiba történt a szerverrel való kommunikáció közben!");
-			}
-		});
-	});
+            success : function(soapResponse) {
+                var xmlStr = soapResponse.toString();
+                var xmlDoc = $.parseXML(xmlStr);
+                $xml = $(xmlDoc);
+                var errorStr = $xml.find("error").text();
+                if (errorStr) {
+                    alert(errorStr);
+                } else {
+                    location.reload();
+                }
+            },
+            error : function(soapResponse) {
+                // show error
+                alert("Hiba történt a szerverrel való kommunikáció közben!");
+            }
+        });
+    });
 
-	$.getJSON("products", function(json) {
-		$("#productsTbody").jPut({
-			jsonData : json,
-			name : "productsTbody_template",
-		});
-		$("#productsSelect").jPut({
-			jsonData : json,
-			name : "productsSelect_template",
-		});
-	});
-	$.getJSON("customers", function(json) {
-		$("#customersSelect").jPut({
-			jsonData : json,
-			name : "customersSelect_template",
-		});
-	});
-	$.getJSON("sales", function(json) {
-		$("#salesTbody").jPut({
-			jsonData : json,
-			name : "salesTbody_template",
-		});
-		$(".needsDateFormatting").html(function() {
-			return new Date(parseInt($(this).html())).toLocaleString();
-		})
-	});
+    $.getJSON("products", function(json) {
+        $("#productsTbody").jPut({
+            jsonData : json,
+            name : "productsTbody_template",
+        });
+        $("#productsSelect").jPut({
+            jsonData : json,
+            name : "productsSelect_template",
+        });
+    });
+    $.getJSON("customers", function(json) {
+        $("#customersSelect").jPut({
+            jsonData : json,
+            name : "customersSelect_template",
+        });
+    });
+    $.getJSON("sales", function(json) {
+        $("#salesTbody").jPut({
+            jsonData : json,
+            name : "salesTbody_template",
+        });
+        $(".needsDateFormatting").html(function() {
+            return new Date(parseInt($(this).html())).toLocaleString();
+        })
+    });
 });
